432349ce9d0c0b5cdeb79984d12f6d874871291d
Ten-Green <GU27081975@yandex.ru>
2023-07-31 23:03:15 +0300

    init

---
+++ src/test/java/com/example/kw_2/Kw2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kw_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kw2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
